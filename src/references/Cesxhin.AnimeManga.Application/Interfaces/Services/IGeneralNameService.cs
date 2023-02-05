ca79787ee3d45bff552db8fd582c1039626ee3ff
Anime-Manga <mattia.ceschin@gmail.com>
2023-02-05 09:34:12 +0000

    Update from https://github.com/Anime-Manga/references/commit/043e858ac1b59b69f8eb89903468b64b1b71a4fc

---
+++ src/references/Cesxhin.AnimeManga.Application/Interfaces/Services/IGeneralNameService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cesxhin.AnimeManga.Application.Interfaces.Services
+{
+    public interface IGeneralNameService<TGeneralNameDTO>
+    {
+        //get
+        Task<IEnumerable<TGeneralNameDTO>> GetNameAllAsync();
+        Task<TGeneralNameDTO> GetNameByNameAsync(string name);
+        Task<IEnumerable<TGeneralNameDTO>> GetMostNameByNameAsync(string name);
+        Task<IEnumerable<TGeneralNameDTO>> GetNameAllWithAllAsync();
+
+        //insert
+        Task<TGeneralNameDTO> InsertNameAsync(TGeneralNameDTO anime);
+
+        //delete
+        Task<string> DeleteNameByIdAsync(string id);
+    }
+}
---
+++ src/references/Cesxhin.AnimeManga.Domain/DTO/DownloadDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Cesxhin.AnimeManga.Domain.DTO
+{
+    public class DownloadDTO
+    {
+        public string Url { get; set; }
+        public string nameCfg { get; set; }
+    }
+}
